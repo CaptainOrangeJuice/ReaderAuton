b1dbd093ae5eaf26f970072e2e2a1e6749b10db5
Captain Orange Juice <bacespaby@gmail.com>
2025-01-06 13:31:32 -0600

    first commit

---
+++ include/motors.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "vex.h"
+#include <iostream>
+#include <fstream>
+#define SCREAM(TEXT) std::cout<<TEXT<<std::endl;
+#define yap(text) std::cout<<text<<std::endl;
+#define printToConsole(text) std::cout<<text<<std::endl;
+#define BOOM(text) std::cout<<TEXT<<std::endl;
+
+inline extern vex::brain Brain = vex::brain();
+inline extern vex::motor Fl = vex::motor(vex::PORT6, vex::ratio18_1, true); 
+inline extern vex::motor Fr = vex::motor(vex::PORT7, vex::ratio18_1, false);
+inline extern vex::motor Tl = vex::motor(vex::PORT11, vex::ratio18_1, false); //Motor on the left side above the others
+inline extern vex::motor Tr = vex::motor(vex::PORT3, vex::ratio18_1, true); //Motor on the right side above the others
+inline extern vex::motor Bl = vex::motor(vex::PORT2, vex::ratio18_1, true);
+inline extern vex::motor Br = vex::motor(vex::PORT1, vex::ratio18_1, false);
+inline extern vex::motor belt = vex::motor(vex::PORT10, vex::ratio18_1, true);
+inline extern vex::motor manlySilverArmL = vex::motor(vex::PORT16, vex::ratio36_1, true);
+inline extern vex::motor manlySilverArmR = vex::motor(vex::PORT13, vex::ratio36_1, false);
+inline extern vex::motor_group manlySilverArm = vex::motor_group(manlySilverArmL, manlySilverArmR); //Not the lady brown frfr
+inline extern vex::digital_out clampPneumatics = vex::digital_out(Brain.ThreeWirePort.H);
+inline extern vex::controller Controller1 = vex::controller(vex::primary);
+inline extern vex::motor_group Left = vex::motor_group(Fl, Tl, Bl);
+inline extern vex::motor_group Right = vex::motor_group(Fr, Tr, Br);
+inline extern vex::inertial InertialSensor = vex::inertial(vex::PORT17);
+inline extern vex::optical OpticalSensor = vex::optical(vex::PORT15);
+inline extern vex::rotation RotationSensor = vex::rotation(vex::PORT14);
+// inline extern vex::rotation rotationL = new vex::rotation(PORT18, false);
+//extern kDeviceTypeMotorSensor
---
+++ src/main.cpp
@@ -0,0 +1,245 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       orang                                                     */
+/*    Created:      12/20/2024, 5:35:26 PM                                    */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include "vex.h"
+#include "motors.h"
+#include <fstream>
+using namespace std;
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+#define LOOP_DELAY 20 //20 ms per loop
+#define START_MERCY ((1000/LOOP_DELAY) * 0) //How many seconds to remove from the beginning [(1000/LOOP_DELAY) is the number of loops per second]
+#define END_MERCY ((1000/LOOP_DELAY) * 4) //Give extra time? [1 second as of now]
+#define MAX_INPUTS ((1000/LOOP_DELAY) * 20) //1000 for 20 seconds
+#define forward vex::forward
+uint8_t inputs[6][MAX_INPUTS];
+uint8_t checkData[6][MAX_INPUTS];
+uint8_t emptyData[0][0];
+/*uint8_t     myTestData[ 100 ];
+uint8_t     myReadBuffer[ 1000 ];*/
+
+    
+    // printToConsole("loop begun");
+    // Brain.SDcard.
+    // ofstream myfile;
+    // myfile.open(Brain.SDcard.appendfile());
+    // myfile.open ("C:\\Users\\orang\\Documents\\test.txt");
+    // myfile << "Writing this to a file." << std::endl;
+    // myfile.close();
+    // printToConsole("loop completed");
+
+void pre_auton(void) {
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) {
+  int nRead = Brain.SDcard.loadfile( "Auton.dat", (uint8_t*)inputs, sizeof(inputs));
+  
+  if (nRead == sizeof(inputs)) {
+    for (int i = 0; i < MAX_INPUTS; i++) {
+      if (i <= START_MERCY) {
+        continue;
+      }
+      int8_t fwd = inputs[0][i];
+      int8_t lr = inputs[1][i];
+      int8_t _slow = inputs[2][i];
+      int8_t beltFwd = inputs[3][i];
+      int8_t beltRev = inputs[4][i];
+      int8_t Pneumatics = inputs[5][i];
+
+      Left.spin(forward, (fwd + lr) * (_slow / 100) * 0.8, pct);
+      Right.spin(forward, (fwd - lr) * (_slow / 100) * 0.8, pct);
+
+      if (beltFwd) belt.spin(forward, 100, pct);
+      else if (beltRev) belt.spin(reverse, 100, pct);
+      else belt.stop(brake);
+
+      if (Pneumatics != inputs[5][i-1]) {
+        clampPneumatics.set(Pneumatics);
+      }
+
+      wait(LOOP_DELAY, msec);
+    }
+  } else { printToConsole("Load failed!"); Controller1.Screen.print("Load failed!"); }
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+  int loopNum = 0;
+  double slow = 1;
+  bool pneumaticsBool = false;
+  bool pressingBool = false;
+  // User control code here, inside the loop
+  /*for (int i = 0; i < 3; i++) {
+    Controller1.rumble(".");
+    wait(1, sec);
+  }
+  Controller1.Screen.print("go!");
+  Controller1.rumble("-");*/
+  while (1) {
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo
+    // values based on feedback from the joysticks.
+
+    // ........................................................................
+    // Insert user code here. This is where you use the joystick values to
+    // update your motors, etc.
+    // ........................................................................
+  //if (Brain.SDcard.isInserted()) {
+    // printToConsole("a");
+    int8_t fwd = Controller1.Axis3.value();
+    int8_t lr = Controller1.Axis1.value();
+    int8_t _slow = slow * 100;
+    int8_t beltFwd = Controller1.ButtonR1.pressing();
+    int8_t beltRev = Controller1.ButtonR2.pressing();
+    int8_t Pneumatics = pneumaticsBool;
+
+    if (loopNum <= (MAX_INPUTS/* + START_MERCY + END_MERCY*/)) {
+      inputs[0][loopNum] = fwd; //Left joystick (vertical)
+      inputs[1][loopNum] = lr; //Right joystick (horizontal)
+      inputs[2][loopNum] = _slow;
+      inputs[3][loopNum] = beltFwd;
+      inputs[4][loopNum] = beltRev;
+      inputs[5][loopNum] = Pneumatics;
+
+      loopNum++;
+    } else {
+      printToConsole("Recording finished.");
+    }
+
+    if (Controller1.ButtonUp.pressing()){
+      slow = 0.4;
+    } else {
+      slow = 1;
+    }
+
+    Left.spin(forward, (Controller1.Axis3.position() + Controller1.Axis1.position()) * slow * 0.8, pct);
+    Right.spin(forward, (Controller1.Axis3.position() - Controller1.Axis1.position()) * slow * 0.8, pct);
+
+
+    if (Controller1.ButtonR1.pressing()) {
+      //to the pen
+      belt.spin(forward, 100, pct);
+    } else if (Controller1.ButtonR2.pressing()) {
+      //to the king
+      belt.spin(reverse, 100, pct);
+    } else {
+      //where's my crown
+      belt.stop(brake);
+    }
+    
+    if (Controller1.ButtonA.pressing()) {
+      //that's my bling
+      if (pressingBool == false) {
+        pneumaticsBool = !pneumaticsBool;
+        clampPneumatics.set(pneumaticsBool);
+        printToConsole(pneumaticsBool);
+      }
+      pressingBool = true;
+    } else {
+      //always drama when I ring
+      pressingBool = false;
+      // printToConsole("Pneumatics set to ");
+    }
+    
+    
+    // printToConsole("b");
+    // for(int i=0;i<25;i++) { Brain.Screen.print(myData[i]); printToConsole(myData[i]); }
+    //Brain.SDcard.savefile( "test", (uint8_t*)myData, sizeof(myData) );
+    if (Controller1.ButtonDown.pressing() && Controller1.ButtonLeft.pressing()) {
+      Brain.SDcard.savefile( "Auton.dat", (uint8_t*)inputs, sizeof(inputs) );
+      printToConsole("Data saved.");
+      wait(1, sec);
+    }
+
+    if (Controller1.ButtonDown.pressing() && Controller1.ButtonRight.pressing()) {
+      Brain.SDcard.savefile( "Auton.dat", (uint8_t*)emptyData, sizeof(emptyData) );
+      int nRead = Brain.SDcard.loadfile( "Auton.dat", (uint8_t*)checkData, sizeof(checkData));
+      printToConsole("DATA RESET. RUN PROGRAM AGAIN TO INPUT DATA.");
+      if (nRead == sizeof(inputs)) {
+        wait(0.5, sec);
+        printToConsole("wait");
+        wait(0.3, sec);
+        printToConsole("never mind it's still there");
+        wait(0.8, sec);
+        printToConsole("uhhhhhhhhhhhhhhhhhhhhhhhhhh");
+        wait(0.4, sec);
+        printToConsole(".");
+        wait(0.1, sec);
+        printToConsole(".");
+        wait(0.1, sec);
+        printToConsole(".");
+        wait(0.5, sec);
+        printToConsole("Just remove it manually idk");
+      }
+      loopNum += MAX_INPUTS + 1;
+      wait(1, sec);
+    }
+
+    // printToConsole("c");
+
+    // int nRead = Brain.SDcard.loadfile( "Auton.txt", (uint8_t*)myData, sizeof(myData));
+
+    // printToConsole("d");
+    /*for (int i = 0; i < nRead; i++) {
+      Brain.Screen.print(readBuffer[i]);
+      printToConsole(readBuffer[i]);
+    }*/
+  /*} else {
+
+  }*/
+    wait(LOOP_DELAY, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
+
