3f7a9c7acb36e767ac97baf95dfc5e2937e3a391
Captain Orange Juice <bacespaby@gmail.com>
2025-01-16 14:24:41 -0600

    a

--- src/main.cpp
+++ src/main.cpp
@@ -54,9 +54,10 @@ void pre_auton(void) {
 /*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
-  int nRead = Brain.SDcard.loadfile( "Auton.dat", (uint8_t*)inputs, sizeof(inputs));
+  int nRead = Brain.SDcard.loadfile( "1082VAuton.dat", (uint8_t*)inputs, sizeof(inputs));
   
   if (nRead == sizeof(inputs)) {
+    
     for (int i = 0; i < MAX_INPUTS; i++) {
       if (i <= START_MERCY) {
         continue;
@@ -180,14 +181,14 @@ void usercontrol(void) {
     // for(int i=0;i<25;i++) { Brain.Screen.print(myData[i]); printToConsole(myData[i]); }
     //Brain.SDcard.savefile( "test", (uint8_t*)myData, sizeof(myData) );
     if (Controller1.ButtonDown.pressing() && Controller1.ButtonLeft.pressing()) {
-      Brain.SDcard.savefile( "Auton.dat", (uint8_t*)inputs, sizeof(inputs) );
+      Brain.SDcard.savefile( "1082VAuton.dat", (uint8_t*)inputs, sizeof(inputs) );
       printToConsole("Data saved.");
       wait(1, sec);
     }
 
     if (Controller1.ButtonDown.pressing() && Controller1.ButtonRight.pressing()) {
-      Brain.SDcard.savefile( "Auton.dat", (uint8_t*)emptyData, sizeof(emptyData) );
-      int nRead = Brain.SDcard.loadfile( "Auton.dat", (uint8_t*)checkData, sizeof(checkData));
+      Brain.SDcard.savefile( "1082VAuton.dat", (uint8_t*)emptyData, sizeof(emptyData) );
+      int nRead = Brain.SDcard.loadfile( "1082VAuton.dat", (uint8_t*)checkData, sizeof(checkData));
       printToConsole("DATA RESET. RUN PROGRAM AGAIN TO INPUT DATA.");
       if (nRead == sizeof(inputs)) {
         wait(0.5, sec);
@@ -211,7 +212,7 @@ void usercontrol(void) {
 
     // printToConsole("c");
 
-    // int nRead = Brain.SDcard.loadfile( "Auton.txt", (uint8_t*)myData, sizeof(myData));
+    // int nRead = Brain.SDcard.loadfile( "1082VAuton.txt", (uint8_t*)myData, sizeof(myData));
 
     // printToConsole("d");
     /*for (int i = 0; i < nRead; i++) {
